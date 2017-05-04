76c76c1a6136c4962e4aca0787b191d50d25ac91
Namdak Tonpa <maxim@synrc.com>
2017-05-04 17:50:31 +0300

    package

---
+++ package.exs
@@ -0,0 +1,27 @@
+defmodule CPS.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :cps,
+     version: "0.4.0",
+     description: "CPS Feed Server 0.4 for KVS",
+     package: package,
+     deps: deps]
+  end
+
+  def application do
+    [mod: {:cps, []}]
+  end
+
+  defp package do
+    [files: ["include", "priv", "samples", "src", "LICENSE", "README.md", "rebar.config"],
+     licenses: ["MIT"],
+     maintainers: ["Andy Martemyanov"],
+     name: :cps,
+     links: %{"GitHub" => "https://github.com/synrc/cps"}]
+  end
+
+  defp deps do
+     [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+end
